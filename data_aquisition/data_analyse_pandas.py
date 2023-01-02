d02b75d4c4df4824e7271de5802f892d3e1073bb
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-01-02 15:43:26 +0100

    Scraping

--- data_aquisition/data_analyse_pandas.py
+++ data_aquisition/data_analyse_pandas.py
@@ -1,28 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+def remove_html_tags(s):
+    new_s = re.sub('\s+', ' ', s)
+    return re.sub("<[^<]+?>", "", new_s)
 def get_data_from_url(url) :
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
-    #Create a list with the data we need, that can be found in the class 'classified-table_data'
+    #get the data we need from the class 'classified-table_data
     info = soup.find_all(class_ = 'classified-table__data')
     info_utf8 = str(info).encode('utf-8')
     info_list = str(info_utf8).split('<td class="classified-table__data"')
-    #clean the data
-    pass
-    #return the cleaned data
+    #Clean the data
+    clean_data = []
+    for elem in info_list :
+        clean_data.append(remove_html_tags(elem))
+    print(clean_data[1:])
+    #return the data cleaned
+    return clean_data[1:]
+
+def data_to_pandas(data) :
     pass
 
-    '''
-    Date de dispo
-    Quartier
-    Annee construction
-    Etage
-    Nombre etage
-    Etat batiment
-    Nombre de facade
-
-    A continuer
-    '''
-def do_a_csv(data) :
-    #do a barr ... do a csv
-    pass
+get_data_from_url('https://www.immoweb.be/en/classified/apartment/for-sale/schaerbeek/1030/10303386')
