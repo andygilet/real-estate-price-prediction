018b5d54fd5833172361fec5d07cfb2a688f9adf
andygilet <andy-gilet@live.be>
2023-01-03 16:35:44 +0100

    upgrade of the scrapper I have upgrade the scrapper to search by province instead of the whole country to get more data

--- data_aquisition/scrapper.py
+++ data_aquisition/scrapper.py
@@ -4,11 +4,31 @@ from webdriver_manager.firefox import GeckoDriverManager
 import numpy as np
 import time
 
-def change_page(driver : webdriver.Firefox, page : int, buy_type : str, postal_code : int) -> bool:
+def change_page(driver : webdriver.Firefox, page : int, buy_type : str, province : str) -> bool:
     try:
-        driver.get(f"https://www.immoweb.be/en/search/house-and-apartment/{buy_type}/liege/{postal_code}?countries=BE&page={page}&orderBy=relevance")
-        if driver.title != "House for sale - Immoweb":
-            raise Exception("Last page attain !")
+        driver.get(f"https://www.immoweb.be/en/search/house-and-apartment/{buy_type}/{province}/province?countries=BE&page={page}&orderBy=relevance")
+        if province == "antwerp" and driver.title != "House and apartment for sale - Antwerp (Province) - Immoweb":
+            raise Exception("Last page attain - Antwerp !")
+        elif province == "limburg" and driver.title != "House and apartment for sale - Limburg (Province) - Immoweb":
+            raise Exception("Last page attain - Limburg !")
+        elif province == "east-flanders" and driver.title != "House and apartment for sale - East Flanders (Province) - Immoweb":
+            raise Exception("Last page attain - East-Flanders !")
+        elif province == "flemish-brabant" and driver.title != "House and apartment for sale - Flemish Brabant (Province) - Immoweb":
+            raise Exception("Last page attain - Flemish-Brabant !")
+        elif province == "west-flanders" and driver.title != "House and apartment for sale - West Flanders (Province) - Immoweb":
+            raise Exception("Last page attain - West-Flanders !")
+        elif province == "walloon-brabant" and driver.title != "House and apartment for sale - Walloon Brabant (Province) - Immoweb":
+            raise Exception("Last page attain - Walloon-Brabant !")
+        elif province == "hainaut" and driver.title != "House and apartment for sale - Hainaut (Province) - Immoweb":
+            raise Exception("Last page attain - Hainaut !")
+        elif province == "liege" and driver.title != "House and apartment for sale - Liège (Province) - Immoweb":
+            raise Exception("Last page attain - Liège !")
+        elif province == "luxembourg" and driver.title != "House and apartment for sale - Luxembourg (Province) - Immoweb":
+            raise Exception("Last page attain - Luxembourg !")
+        elif province == "namur" and driver.title != "House and apartment for sale - Namur (Province) - Immoweb":
+            raise Exception("Last page attain - Namur !")
+        elif province == "brussels" and driver.title != "House and apartment for sale - Brussels (Province) - Immoweb":
+            raise Exception("Last page attain - Brussels !")
     except Exception as e:
         print(e)
         return False
@@ -43,24 +63,34 @@ def get_urls() -> list:
     continue_loop : bool = True
     page = 1
     urls = []
-    buy_types = ["for-sale","for-rent"]
+    buy_types = ["for-sale",
+                 "for-rent"]
+    provinces = ["antwerp",
+                 "limburg",
+                 "east-flanders",
+                 "flemish-brabant",
+                 "west-flanders",
+                 "walloon-brabant",
+                 "hainaut",
+                 "liege",
+                 "luxembourg",
+                 "namur",
+                 "brussels"]
     driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    while continue_loop:
-        continue_loop = change_page(driver, page, "for-sale")
-        if continue_loop:
-            try_cookie(driver)
-            print(f"page {page}")
-            get_property_urls(driver, urls)
-            page += 1
-    page = 1
-    while continue_loop:
-        continue_loop = change_page(driver, page, "for-rent")
-        if continue_loop:
-            try_cookie(driver)
-            print(f"page {page}")
-            get_property_urls(driver, urls)
-            page += 1
     
+    for province in provinces:
+        for buy_type in buy_types:
+            while continue_loop:
+                continue_loop = change_page(driver, page, buy_type, province)
+                if continue_loop:
+                    try_cookie(driver)
+                    print(f"page {page}")
+                    get_property_urls(driver, urls)
+                    page += 1
+            page = 1
+            driver.close()
+            driver = webdriver.Firefox()
+            
     print(f"You have {len(urls)} properties !")
     create_csv_file(urls)
     return urls
