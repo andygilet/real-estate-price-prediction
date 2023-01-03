921ce6f85c075f84686d5cd902f15aae9bff12a8
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-01-03 15:16:46 +0100

    Project almost finished

--- data_aquisition/data_cleaning.py
+++ data_aquisition/data_cleaning.py
@@ -1,26 +1,27 @@
-from data_analyse_pandas import get_data_from_url, data_to_pandas
-needed_data_dic = {
-    'locality' : [],
-    'Type_property' : [],
+import re
+def reference_dic_needed(the_frame):
+    needed_data_dic = {
+    'locality' : 'locality',
+    'Type_property' : 'property_type',
     'Price' : 'Price, ',
-    'Sale_type' : [],
-    'Number_rooms' : [],
+    'Sale_type' : 'sale_type',
+    'Number_bedrooms' : "bedroom",
     'Living_area' : '\\n Living area\\n , ',
     'fully_equipped_kitchen' : 'Kitchen type, ',#Yes no
     'Furnished' : 'Furnished, ',
     'terrace' : 'Terrace surface, ',
     'garden' : 'Garden surface, ', 
-    'surface_land' : [],
+    'surface_land' : 'size_of_house',
     'surface_area_plot' : 'Surface of the plot, ',
     'facades_number' : '\\n Number of frontages\\n , ',
     'Swimming_pool' : '\\n Swimming pool\\n , ',
     'building_state' : '\\n Building condition\\n , '
-}
-the_frame = {}
-the_data = get_data_from_url("https://www.immoweb.be/en/classified/apartment-block/for-sale/ans/4430/10300959")
+    }
+    last_frame = {}
+    for key,value in needed_data_dic.items() :
+        last_frame[key] = the_frame[value]
 
-the_frame = data_to_pandas(the_data,the_frame)
-the_data = get_data_from_url("https://www.immoweb.be/en/classified/house/for-sale/molenbeek-saint-jean/1080/10303905")
-the_frame = data_to_pandas(the_data,the_frame)
-
-print(the_frame)
+    
+    return last_frame
+def clean_escape_characters(string):
+    return re.sub(r'\\n|\\x..', '', string)
---
+++ main.py
@@ -0,0 +1,26 @@
+from data_aquisition.data_analyse_pandas import get_data_from_url, data_to_pandas
+from data_aquisition.data_cleaning import reference_dic_needed, clean_escape_characters
+import pandas as pd
+import csv
+def main() :
+    ploted_frame = {}
+    count = 0
+    with open('links.csv', 'r') as links_file : 
+        reader = csv.reader(links_file)
+        for row in reader : 
+            the_data = get_data_from_url(row[0])
+            ploted_frame = data_to_pandas(the_data,ploted_frame)
+            count += 1
+            print(f"I'm at {count} page done")
+            if count == 500 : 
+                break
+    for key in ploted_frame:
+        ploted_frame[key] = [clean_escape_characters(value) for value in ploted_frame[key]]
+    ploted_frame = reference_dic_needed(ploted_frame)
+
+    df = pd.DataFrame(ploted_frame)
+
+    df.to_csv('test.csv')
+
+if __name__ == '__main__' :
+    main()
