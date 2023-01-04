16a543b06e3eab9f9559a8a1743426175e414738
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-01-05 00:00:14 +0100

    still thread

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def data_to_csv(pass_row) :
             
 def main() :
     with open('immo_data.csv', 'w') as data_file : 
-        data_file.write(',locality,Type_property,Price,Sale_type,Number_bedrooms,Living_area,fully_equipped_kitchen,Furnished,terrace,garden,surface_land,surface_area_plot,facades_number,Swimming_pool,building_state,fire_place')
+        data_file.write(',locality,Type_property,Price,Sale_type,Number_bedrooms,Living_area,fully_equipped_kitchen,Furnished,terrace,garden,surface_land,surface_area_plot,facades_number,Swimming_pool,building_state,fire_place\n')
     thread = threading.Thread(target = data_to_csv,args =(0,))
     thread2 = threading.Thread(target = data_to_csv,args =(150,))
     thread.start()
