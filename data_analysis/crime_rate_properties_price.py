0a1ac245a215a7d853e0ae53ad29c3797e87107b
andygilet <andy-gilet@live.be>
2023-01-17 10:05:32 +0100

    final code of part 2

--- data_analysis/crime_rate_properties_price.py
+++ data_analysis/crime_rate_properties_price.py
@@ -5,20 +5,50 @@ from statistics import median
 import matplotlib.pyplot as plt
 
 def create_graph_price_difference_renovated(dataFrame : pd.DataFrame):
+    """_summary_
+
+    Args:
+        dataFrame (pd.DataFrame): _description_
+    
+    Description:
+        This function create the graph for the question :
+        In which province is it more beneficial to renovate properties?
+    """
     sns.catplot(data= dataFrame, kind="bar", x="Provinces", y="Price", hue="Status")
     plt.title("Comparison between the median price of new or renovated properties to to be renovated properties")
     plt.xlabel("Provinces")
+    plt.xticks(rotation=45)
     plt.ylabel("Prices(€)")
     plt.show()
     
 def create_graph_compare_price_crime_rates(dataFrame : pd.DataFrame):
+    """_summary_
+
+    Args:
+        dataFrame (pd.DataFrame): _description_
+        
+    Description:
+        This function create the graph for the question :
+        Is the crimes rates of a province in Belgium influancing the price of the properties in that province ?
+    """
+    sns.scatterplot(x="Crimes Rates", y="Price", data=dataFrame)
     plt.title("Median price of properties by provinces compare to the crime rates")
-    graph = sns.regplot(data = dataFrame, x = "Crimes Rates", y = "Price",
-                        fit_reg = True,)
-    graph.set_axis_labels("Crime rates", "Prices(€)")
+    plt.xlabel("Crimes Rates (for 10 000 habitants)")
+    plt.ylabel("Prices(€)")
     plt.show()
     
 def transform_locality_to_provinces(postal_code : str) -> str:
+    """_summary_
+
+    Args:
+        postal_code (str): _description_
+
+    Returns:
+        str: _description_
+        
+    Description:
+        This function take a postal code and return the province in belgium it belongs to
+    """
     temp = int(postal_code)
     if temp >= 1000 and temp <= 1299:
         return "Bruxelles"
@@ -46,6 +76,18 @@ def transform_locality_to_provinces(postal_code : str) -> str:
         return "Error"
     
 def create_dataset_price_difference_renovated(dataFrame : pd.DataFrame) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        dataFrame (pd.DataFrame): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+        
+    Description:
+        This function take the brut dataset of immoweb to return a new dataset that can be used to answer the question : 
+        In which province is it more beneficial to renovate properties?
+    """
     new_dataFrame = pd.DataFrame()
 
     type_property = []
@@ -92,6 +134,17 @@ def create_dataset_price_difference_renovated(dataFrame : pd.DataFrame) -> pd.Da
     return new_dataFrame
 
 def immoweb_median_price(immoweb_data : pd.DataFrame) -> list:
+    """_summary_
+
+    Args:
+        immoweb_data (pd.DataFrame): _description_
+
+    Returns:
+        list: _description_
+        
+    Description:
+        This function returns a list containing the median price of the properties on each province in belgium.
+    """
     locality = []
     price = []
     type_property = []
@@ -128,6 +181,21 @@ def immoweb_median_price(immoweb_data : pd.DataFrame) -> list:
                 
             
 def create_dataset_compare_price_crime_rates(immoweb_data : pd.DataFrame, demography_data : pd.DataFrame, crimes_data : pd.DataFrame) -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        immoweb_data (pd.DataFrame): _description_
+        demography_data (pd.DataFrame): _description_
+        crimes_data (pd.DataFrame): _description_
+
+    Returns:
+        pd.DataFrame: _description_
+        
+    Description:
+        This function take the brut dataset of immoweb, the demography in belgium, and the crimes in belgium
+        to return a new dataset that can be used to answer the question : 
+        Is the crimes rates of a province in Belgium influancing the price of the properties in that province ?
+    """
     new_dataFrame = pd.DataFrame()
     provinces = []
     habitants = []
@@ -147,21 +215,40 @@ def create_dataset_compare_price_crime_rates(immoweb_data : pd.DataFrame, demogr
     mediane_prices = immoweb_median_price(immoweb_data)
     
     for i in range(len(provinces)):
-        new_row = pd.Series({"Provinces" : provinces[i], "Price" : mediane_prices[i], "Crimes Rates" : float(total_crimes[i]) / habitants[i] * 10000})
+        new_row = pd.Series({"Provinces" : provinces[i], "Price" : int(mediane_prices[i]), "Crimes Rates" : int(float(total_crimes[i]) / habitants[i] * 10000)})
         new_dataFrame = pd.concat([new_dataFrame, new_row.to_frame().T], ignore_index=True)
         
     return new_dataFrame
 
+def create_graph(price_renovated_dataframe : pd.DataFrame, price_crime_rate : pd.DataFrame):
+    """_summary_
+
+    Args:
+        price_renovated_dataframe (pd.DataFrame): _description_
+        price_crime_rate (pd.DataFrame): _description_
+        
+    Description : 
+        Call the functions that creates the plots and show them in a window.
+    """
+    create_graph_price_difference_renovated(price_renovated_dataframe)
+    create_graph_compare_price_crime_rates(price_crime_rate)
+    plt.show()
+
 def immoweb_data_to_graph():
+    """_summary_
+    
+        Description : 
+            The function to call to generate the plots for my two questions : Is the crimes rates of a province in Belgium influancing the price of the properties
+            in that province ? And in which province is it more beneficial to renovate properties?
+            For that i've imported two more dataset, one on the criminality in belgium in 2021 and another one about the demography in belgium.
+    """
     immoweb_dataframe = pd.read_csv(".\immo_data.csv")
     crime_dataframe = read_ods(".\criminality_belgium_2021.ods")
     demography_belgium = read_ods(".\demographie_belgique_2021.ods")
     
     price_renovated_dataframe = create_dataset_price_difference_renovated(immoweb_dataframe)
     price_crime_rate = create_dataset_compare_price_crime_rates(immoweb_dataframe, demography_belgium, crime_dataframe)
+    create_graph(price_renovated_dataframe, price_crime_rate)
     
-    create_graph_price_difference_renovated(price_renovated_dataframe)
-    print(price_crime_rate)
-    #create_graph_compare_price_crime_rates(price_crime_rate)
 
 immoweb_data_to_graph()
